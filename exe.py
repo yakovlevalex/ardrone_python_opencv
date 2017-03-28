eb24a5a2c8e6ceb17cb013416a577237ebf2a36b
user <yakso@bk.ru>
2017-03-28 14:06:46 +0300

    hand posture simple recognition

---
+++ exe.py
@@ -0,0 +1,42 @@
+#!/home/user/.virtualenvs/cv/bin/python2.7 python
+
+# import numpy as np
+# import cv2
+# img = cv2.imread('games.jpg',0)
+# cv2.imshow('image',img)
+# k = cv2.waitKey(0)
+# if k == 27:         # wait for ESC key to exit
+#     cv2.destroyAllWindows()
+# elif k == ord('s'): # wait for 's' key to save and exit
+#     cv2.imwrite('games.png',img)
+#     cv2.destroyAllWindows()
+
+# import numpy as np
+# import cv2
+# from matplotlib import pyplot as plt
+
+# img = cv2.imread('games.jpg',0)
+# plt.imshow(img, cmap = 'gray', interpolation = 'bicubic')
+# plt.xticks([]), plt.yticks([])  # to hide tick values on X and Y axis
+# plt.show()
+
+import numpy as np
+import cv2
+
+cap = cv2.VideoCapture(0)
+
+while(True):
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+
+    # Our operations on the frame come here
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    # Display the resulting frame
+    cv2.imshow('frame',gray)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# When everything done, release the capture
+cap.release()
+cv2.destroyAllWindows()
